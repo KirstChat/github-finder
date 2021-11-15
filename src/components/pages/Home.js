71a1ccdf75cc8231c9108ff0ac9f805d167e2b19
Kirsty Chatterton <chattertonkirsty@gmail.com>
2021-11-15 21:57:23 +0000

    add context

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react/cjs/react.production.min';
+import Search from '../users/Search';
+import Users from '../users/Users';
+
+const Home = () => {
+  return (
+    <Fragment>
+      <Search />
+      <Users />
+    </Fragment>
+  );
+};
+
+export default Home;
